c5f5af65411d6d78846e5f89c3b47a071be38814
Robert Xue <rxue@groupon.com>
2018-01-18 15:44:05 -0600

    Update doc

--- core/src/main/scala/com/roboxue/niffler/Formula.scala
+++ core/src/main/scala/com/roboxue/niffler/Formula.scala
@@ -1,6 +1,13 @@
 package com.roboxue.niffler
 
 /**
+  * Helper class to describe how to evaluate a value in runtime
+  * Don't create this class directly. Use helper functions in [[Token.asFormula]], [[Token.mapFormula()]],
+  * [[com.roboxue.niffler.syntax.Requires]] and [[com.roboxue.niffler.syntax.Constant]]
+  *
+  *
+  * @param prerequisites the tokens that it depends on (has to finish evaluating before this starts)
+  * @param evalF the function to execute when it's prerequisites has finished
   * @author rxue
   * @since 12/27/17.
   */
